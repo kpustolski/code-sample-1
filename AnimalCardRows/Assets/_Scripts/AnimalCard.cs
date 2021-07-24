d4f9dbf4a87320e3e9debf47cf6afc716bf973ed
Katie Pustolski <pustolskik@gmail.com>
2021-07-24 15:26:57 -0700

    Added more setup code to AnimalCard and AnimalRow

--- AnimalCardRows/Assets/_Scripts/AnimalCard.cs
+++ AnimalCardRows/Assets/_Scripts/AnimalCard.cs
@@ -12,10 +12,14 @@ namespace CodeSampleOne
         private Image cardImage = default;
         [SerializeField]
         private TextMeshProUGUI titleText = default;
+        private string id = default;
 
-        public void Setup()
+        public void Setup(string id, string name)
         {
+            id = this.id;
+            titleText.text = name;
 
+            //TODO: Set up the image
         }
 
         public void Shutdown()
--- AnimalCardRows/Assets/_Scripts/AnimalRow.cs
+++ AnimalCardRows/Assets/_Scripts/AnimalRow.cs
@@ -15,18 +15,25 @@ namespace CodeSampleOne
 
         private List<AnimalCard> animalCardList = new List<AnimalCard>();
         private AppManager appMan = default;
+        private List<Animal> mAnimalList = new List<Animal>();
 
-        public void Setup(string title)
+        public void Setup(string title, List<Animal> animalData)
         {
             appMan = AppManager.Instance;
             titleText.text = title;
+            mAnimalList = animalData;
+
+            CreateAnimalCards();
         }
 
-        public void CreateAnimalCard()
+        public void CreateAnimalCards()
         {
-            AnimalCard card = Instantiate(appMan.AnimalCardPrefab, cardParent);
-            card.Setup();
-            animalCardList.Add(card);
+            foreach (var a in mAnimalList)
+            {
+                AnimalCard card = Instantiate(appMan.AnimalCardPrefab, cardParent);
+                card.Setup(id: a.id, name: a.name);
+                animalCardList.Add(card);
+            }
         }
 
         public void Shutdown()

0e7de9fb7d892017824438d87aac16ebe326f211
Katie Pustolski <pustolskik@gmail.com>
2021-07-24 12:08:53 -0700

    Created AppManager script and made an AppManager game object in main scene.

---
+++ AnimalCardRows/Assets/_Scripts/AppManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeSampleOne
+{
+    public class AppManager : MonoBehaviour
+    {
+        [Header("Views")]
+        [Space(10)]
+        [SerializeField]
+        private HomeView homeView = default;
+
+        [Header("Prefabs")]
+        [Space(10)]
+        [SerializeField]
+        private AnimalRow animalRowPrefab = default;
+        [SerializeField]
+        private AnimalCard animalCardPrefab = default;
+        public AnimalRow AnimalRowPrefab { get { return animalRowPrefab; } }
+        public AnimalRow AnimalCard { get { return animalCardPrefab; } }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Grab the data
+            //Setup the home view
+        }
+    }
+}

3326571a1e54dd0f4d137e23a7e2c63b08de6bdb
Katie Pustolski <pustolskik@gmail.com>
2021-07-24 12:05:49 -0700

    Created Animal class and Utilities class.

---
+++ AnimalCardRows/Assets/_Scripts/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeSampleOne
+{
+    public enum AnimalType
+    {
+        None = 0,
+        Dog = 1,
+        Cat = 2,
+        Lizard = 3
+    }
+
+    public class Animal
+    {
+        public string Id;
+        public string Name;
+        public AnimalType Type;
+    }
+}

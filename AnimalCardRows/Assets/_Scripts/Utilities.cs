f7a542a73cdf4d29c3bebc20088a9b2d079e0298
Katie Pustolski <pustolskik@gmail.com>
2021-07-25 22:28:18 -0700

    Moved debug code into Utilities. Consolidated Validate function with AddToAnimalList function in AppManager.cs.

--- AnimalCardRows/Assets/_Scripts/Utilities.cs
+++ AnimalCardRows/Assets/_Scripts/Utilities.cs
@@ -6,5 +6,26 @@ namespace CodeSampleOne
 {
     public static class Utilities
     {
+
+        #region Debug Functions
+        // Creates animal objects to debug with.
+        public static List<Animal> DebugAnimals()
+        {
+            List<Animal> tempList = new List<Animal>();
+            Animal a1 = new Animal("animal1", "Katara", AnimalType.Dog);
+            Animal a2 = new Animal("animal2", "Korra", AnimalType.Cat);
+            Animal a3 = new Animal("animal3", "Aang", AnimalType.Lizard);
+            Animal a4 = new Animal("animal4", "Asami", AnimalType.Lizard);
+
+            tempList.Add(a1);
+            tempList.Add(a2);
+            tempList.Add(a3);
+            tempList.Add(a4);
+
+            return tempList;
+
+        }
+        #endregion
+
     }
 }

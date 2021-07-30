0b7a26193e5ea306d39716ded2e4304f7d3bbc8c
Katie Pustolski <pustolskik@gmail.com>
2021-07-29 20:26:09 -0700

    Added show and hide animations to the InfoModalTemplate popup.

--- AnimalCardRows/Assets/_Scripts/Modals/InfoModalTemplate.cs
+++ AnimalCardRows/Assets/_Scripts/Modals/InfoModalTemplate.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using DG.Tweening;
 
 namespace CodeSampleOne
 {
@@ -16,6 +17,22 @@ namespace CodeSampleOne
         [SerializeField]
         private TextMeshProUGUI titleText = default;
 
+        [Header("Canvas Groups")]
+        [Space(5)]
+        [SerializeField]
+        private CanvasGroup overlayCanvasGroup = default;
+        [SerializeField]
+        private CanvasGroup contentPanelCanvasGroup = default;
+        [SerializeField]
+        private CanvasGroup buttonCanvasGroup = default;
+
+        // Animation variables
+        private const float overlayFadeDuration = 0.2f;
+        private const float contentFadeDuration = 0.2f;
+        private const float buttonFadeDuration = 0.2f;
+        private Vector3 contentScalePunch = new Vector3(0.1f, 0.1f, 0.1f);
+        private const float contentScaleDuration = 0.2f;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -23,22 +40,42 @@ namespace CodeSampleOne
 
         public void Setup(Animal a, UnityAction cbOnSelect)
         {
-            animalImage.sprite = a.AniSprite;
-            descText.text = a.description;
-            titleText.text = a.name;
+            overlayCanvasGroup.alpha = 0;
+            contentPanelCanvasGroup.alpha = 0;
+            buttonCanvasGroup.alpha = 0;
+            RectTransform contentRectTransform = contentPanelCanvasGroup.gameObject.GetComponent<RectTransform>();
 
-            if (cbOnSelect != null)
+            Sequence showSequence = DOTween.Sequence();
+            showSequence.AppendCallback(() =>
             {
-                cbOnSelect();
-            }
+                animalImage.sprite = a.AniSprite;
+                descText.text = a.description;
+                titleText.text = a.name;
 
-            Initialize();
+                if (cbOnSelect != null)
+                {
+                    cbOnSelect();
+                }
+
+                Initialize();
+            })
+            .Append(overlayCanvasGroup.DOFade(1f, overlayFadeDuration))
+            .Append(contentPanelCanvasGroup.DOFade(1f, contentFadeDuration))
+            .Join(contentRectTransform.DOPunchScale(contentScalePunch, contentScaleDuration))
+            .Append(buttonCanvasGroup.DOFade(1f, buttonFadeDuration));
         }
 
         public override void Shutdown()
         {
-            base.Shutdown();
-            Destroy(gameObject);
+            Sequence hideSequence = DOTween.Sequence();
+            hideSequence.Append(buttonCanvasGroup.DOFade(0f, buttonFadeDuration))
+                .Append(contentPanelCanvasGroup.DOFade(0f, contentFadeDuration))
+                .Append(overlayCanvasGroup.DOFade(0f, overlayFadeDuration))
+                .OnComplete(() =>
+                {
+                    base.Shutdown();
+                    Destroy(gameObject);
+                });
         }
     }
 }
